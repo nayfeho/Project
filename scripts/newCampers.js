549013720c8d89dcbd6b20a28089887fdfbed4e5
Omar <nayfeho1@gmail.com>
2023-11-09 16:55:42 -0500

    intial

---
+++ scripts/newCampers.js
@@ -0,0 +1,224 @@
+////////////////////////
+//--------------------//
+//  NEW RVS ONLY!!!   //
+//  for newUsed:      //
+//  new = 30          //
+//  used = 15         //
+//--------------------//
+////////////////////////
+
+var myArray = {
+    "campers": [
+        {
+            "img": "https://assets-cdn-interactrv.netdna-ssl.com/interactrv/unit_tech_drawing/imgh_800x600-m1016202013184607/unit_tech_drawing_202010160118460698594900.jpg",
+            "img2": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0615202120523606/unit_photo_202106150852360621857498.jpg",
+            "img3": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0615202120524951/unit_photo_202106150852495119498545.jpg",
+            "type": "Fifth Wheel",
+            "name": "New 2021 Palomino Puma 257RESS",
+            "url": "https://www.asrvcenter.com/product/new-2021-palomino-puma-257ress-1532612-5",
+            "selection": "Price: $33,995",
+            "location": "Midland, MI",
+            "vinNum": "4X4FPUB26MP091651",
+            "price": 33995,
+            "lastOfYear": 21,
+            "extWidth": 8,
+            "extHeight": 12.2,
+            "newUsed": 30
+       },
+        {
+            "img": "https://assets-cdn-interactrv.netdna-ssl.com/interactrv/unit_tech_drawing/imgh_800x600-m0119202118472046/unit_tech_drawing_202101190647204636157852.jpg",
+            "img2": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0917202119435093/unit_photo_202109170743509361698371.jpg",
+            "img3": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0917202119435178/unit_photo_202109170743517880936134.jpg",
+            "type": "Travel Trailer",
+            "name": "New 2022 Keystone RV Hideout Single Axle 175BH",
+            "url": "https://www.asrvcenter.com/product/new-2022-keystone-rv-hideout-single-axle-175bh-1612566-29",
+            "selection": "Price: $17,995",
+            "location": "Midland, MI",
+            "vinNum": "4YDTH1G13NW240221",
+            "price": 17995,
+            "lastOfYear": 22,
+            "extWidth": 8,
+            "extHeight": 10.2,
+            "newUsed": 30
+       },
+        {
+            "img": "https://assets-cdn-interactrv.netdna-ssl.com/interactrv/unit_tech_drawing/imgh_800x600-m0818202113291587/unit_tech_drawing_202108180129158730499702.jpg",
+            "img2": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0702202118441736/unit_photo_202107020644173655335331.jpg",
+            "img3": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0702202118443809/unit_photo_202107020644380883596681.jpg",
+            "type": "Fifth Wheel",
+            "name": "New 2021 Palomino Puma 295BHSS",
+            "url": "https://www.asrvcenter.com/product/new-2021-palomino-puma-295bhss-1554180-5",
+            "selection": "Price: $34,995",
+            "location": "Midland, MI",
+            "vinNum": "4X4FPUF22MP092161",
+            "price": 34995,
+            "lastOfYear": 21,
+            "extWidth": 8,
+            "extHeight": 12.4,
+            "newUsed": 30
+       },
+        {
+            "img": "https://assets-cdn-interactrv.netdna-ssl.com/interactrv/unit_tech_drawing/imgh_800x600-m0614202117192773/unit_tech_drawing_202106140519277308127270.jpg",
+            "img2": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0802202121540105/unit_photo_202108020954010484977464.jpg",
+            "img3": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0802202121540202/unit_photo_20210802095402020579417.jpg",
+            "type": "Fifth Wheel",
+            "name": "New 2021 Keystone RV Cougar Half-Ton 32BHS",
+            "url": "https://www.asrvcenter.com/product/new-2021-keystone-rv-cougar-half-ton-32bhs-1570129-5",
+            "selection": "Price: $52,995",
+            "location": "Auburn Hills, MI",
+            "vinNum": "4YDF32B20M2513218",
+            "price": 52995,
+            "lastOfYear": 21,
+            "extWidth": 8,
+            "extHeight": 12.2,
+            "newUsed": 30
+       },
+        {
+            "img": "https://assets-cdn-interactrv.netdna-ssl.com/interactrv/unit_tech_drawing/imgh_800x600-m0630202113512016/unit_tech_drawing_202106300151201606310249.jpg",
+            "img2": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m1103202115134931/unit_photo_202111030313493137309882.jpg",
+            "img3": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m1103202115135032/unit_photo_202111030313503270524973.jpg",
+            "type": "Fifth Wheel",
+            "name": "New 2022 Forest River RV Sabre 37FLL",
+            "url": "https://www.asrvcenter.com/product/new-2022-forest-river-rv-sabre-37fll-1652002-5",
+            "selection": "Price: $69,995",
+            "location": "Midland, MI",
+            "vinNum": "5ZT3SR2B8N6109061",
+            "price": 69995,
+            "lastOfYear": 22,
+            "extWidth": 8,
+            "extHeight": 13.5,
+            "newUsed": 30
+       },
+        {
+            "img": "https://assets-cdn-interactrv.netdna-ssl.com/interactrv/unit_tech_drawing/imgh_800x600-m0818202113291620/unit_tech_drawing_202108180129161984693859.jpg",
+            "img2": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m1011202118262191/unit_photo_202110110626219095269723.jpg",
+            "img3": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m1011202118262044/unit_photo_202110110626204426072710.jpg",
+            "type": "Travel Trailer",
+            "name": "New 2022 Keystone RV Hideout Single Axle 177RD",
+            "url": "https://www.asrvcenter.com/product/new-2022-keystone-rv-hideout-single-axle-177rd-1634518-29",
+            "selection": "Price: $17,995",
+            "location": "Auburn Hills, MI",
+            "vinNum": "4YDTH1G18NW240747",
+            "price": 17995,
+            "lastOfYear": 22,
+            "extWidth": 8,
+            "extHeight": 10.2,
+            "newUsed": 30
+       },
+        {
+            "img": "https://assets-cdn-interactrv.netdna-ssl.com/interactrv/unit_tech_drawing/imgh_800x600-m0630202117470597/unit_tech_drawing_202106300547059726553489.jpg",
+            "img2": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m1028202113353707/unit_photo_202110280135370698485623.jpg",
+            "img3": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m1028202113353669/unit_photo_20211028013536693767211.jpg",
+            "type": "Fifth Wheel",
+            "name": "New 2022 Forest River RV Rockwood Ultra Lite 2442BS",
+            "url": "https://www.asrvcenter.com/product/new-2022-forest-river-rv-rockwood-ultra-lite-2442bs-1650281-5",
+            "selection": "Price: $45,995",
+            "location": "Auburn Hills, MI",
+            "vinNum": "4X4FRLE27N1899110",
+            "price": 45995,
+            "lastOfYear": 22,
+            "extWidth": 8,
+            "extHeight": 13.1,
+            "newUsed": 30
+       },
+        {
+            "img": "https://assets-cdn-interactrv.netdna-ssl.com/interactrv/unit_tech_drawing/imgh_800x600-m1007202113394191/unit_tech_drawing_202110070139419107057619.png",
+            "img2": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m1019202122120804/unit_photo_202110191012080403862917.jpg",
+            "img3": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m1019202122114928/unit_photo_202110191011492805901571.jpg",
+            "type": "Fifth Wheel",
+            "name": "New 2022 Keystone RV Cougar Half-Ton 23MLS",
+            "url": "https://www.asrvcenter.com/product/new-2022-keystone-rv-cougar-half-ton-23mls-1644243-5",
+            "selection": "Price: $49,995",
+            "location": "Auburn Hills, MI",
+            "vinNum": "4YDFCGK25NV501675",
+            "price": 49995,
+            "lastOfYear": 22,
+            "extWidth": 8,
+            "extHeight": 12,
+            "newUsed": 30
+       },
+    ]
+}
+buildTable(myArray)
+
+function buildTable(data) {
+    var table = document.getElementById('myTable')
+
+    for (var i = 0; i < myArray.campers.length; i++) {
+
+        let grade = "null"
+        let gradeNum = 0
+        let price = myArray.campers[i].price
+
+        var calcPrice =
+            myArray.campers[i].extHeight +
+            myArray.campers[i].extWidth +
+            myArray.campers[i].newUsed +
+            myArray.campers[i].lastOfYear
+        calcPrice = calcPrice.toFixed(0)
+
+        let finPrice = (price / calcPrice)
+        finPrice = finPrice.toFixed(0)
+
+        //grade calculator
+        if (finPrice >= 500) {
+            grade = "A"
+            gradeNum = 4
+        } else if (finPrice >= 450) {
+            grade = "B"
+            gradeNum = 3
+        } else if (finPrice >= 350) {
+            grade = "C"
+            gradeNum = 2
+        } else if (finPrice >= 250) {
+            grade = "D"
+            gradeNum = 1
+        } else if (finPrice >= 150) {
+            grade = "F"
+            gradeNum = 0
+        } else {
+            console.log("Error: check grade calculator")
+        }
+
+        var row = (
+            `<tr>
+                <td id="tdTitle" colspan=3><a href=${myArray.campers[i].url}>${myArray.campers[i].type} ${myArray.campers[i].name}</a></td>
+            </tr>
+            <tr>
+                <td class="sortPrice" id="tdTitle" colspan=3>${myArray.campers[i].selection}</td>
+            </tr>
+            <tr>
+                <td>Located in ${myArray.campers[i].location}</td>
+                <td>VIN: ${myArray.campers[i].vinNum}</td>            
+                <td class="tdGrade">Grade: ${grade}</td>
+            </tr>
+            <tr>
+                <td><img src=${myArray.campers[i].img} alt=""></td>
+                <td><img src=${myArray.campers[i].img2} alt=""></td>
+                <td><img src=${myArray.campers[i].img3} alt=""></td>
+            </tr>
+            <tr>
+            </tr>`
+        )
+
+        table.innerHTML += row
+
+        //conditional formatting for cell that displays grade
+        var cell = document.getElementsByClassName("tdGrade")
+        var cellGrade = cell.item(i)
+        var x = cellGrade.innerHTML;
+        if (x == "Grade: A") {
+            cellGrade.style.color = '#38f244'
+        } else if (x == "Grade: B") {
+            cellGrade.style.color = '#a4f238'
+        } else if (x == "Grade: C") {
+            cellGrade.style.color = '#ecf238'
+        } else if (x == "Grade: D") {
+            cellGrade.style.color = '#e87638'
+        } else if (x == "Grade: F") {
+            cellGrade.style.color = '#e85538'
+        } else {
+            console.log("ERROR: check conditional formatting code")
+        }
+    }
+}
---
+++ scripts/usedCampers.js
@@ -0,0 +1,219 @@
+////////////////////////
+//--------------------//
+//  USED RVS ONLY!!!  //
+//  for newUsed:      //
+//  new = 30          //
+//  used = 15         //
+//--------------------//
+////////////////////////
+
+var myArray = {
+    "campers": [
+        {
+            "img": "https://assets-cdn-interactrv.netdna-ssl.com/interactrv/unit_tech_drawing/imgh_800x600-m1202201616043896/mod36720.jpg",
+            "img2": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0723202116492404/unit_photo_202107230449240426463371.jpg",
+            "img3": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0723202116492454/unit_photo_202107230449245394483875.jpg",
+            "type": "Fifth Wheel",
+            "name": "Used 2011 Keystone RV Sydney 322FRL",
+            "url": "https://www.asrvcenter.com/product/used-2011-keystone-rv-sydney-322frl-1548216-5",
+            "selection": "Price: $19,995",
+            "location": "Midland, MI",
+            "vinNum": "4YDF32227BB630551",
+            "price": 19995,
+            "lastOfYear": 11,
+            "extWidth": 8,
+            "extHeight": 12.4,
+            "newUsed": 15
+       },
+        {
+            "img": "https://assets-cdn-interactrv.netdna-ssl.com/interactrv/unit_tech_drawing/imgh_800x600-m1202201616043896/mod63631.jpg",
+            "img2": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m1005202118055475/unit_photo_202110050605547566193623.jpg",
+            "img3": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m1005202118060610/unit_photo_202110050606061056215799.jpg",
+            "type": "Fifth Wheel",
+            "name": "Used 2015 Grand Design Reflection 27RL",
+            "url": "https://www.asrvcenter.com/product/used-2015-grand-design-reflection-27rl-1628317-5",
+            "selection": "Price: $27,995",
+            "location": "Midland, MI",
+            "vinNum": "573FR3122F3302908",
+            "price": 27995,
+            "lastOfYear": 15,
+            "extWidth": 8,
+            "extHeight": 12,
+            "newUsed": 15
+       },
+        {
+            "img": "https://assets-cdn-interactrv.netdna-ssl.com/interactrv/unit_tech_drawing/imgh_800x600-m1202201616043896/mod52404.jpg",
+            "img2": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0927202114490934/unit_photo_202109270249093402366599.jpg",
+            "img3": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0927202114491984/unit_photo_202109270249198462266156.jpg",
+            "type": "Fifth Wheel",
+            "name": "Used 2014 Forest River RV Blue Ridge 3125RT",
+            "url": "https://www.asrvcenter.com/product/used-2014-forest-river-rv-blue-ridge-3125rt-1616263-5",
+            "selection": "Price: $35,995",
+            "location": "Midland, MI",
+            "vinNum": "4X4FBLG25EG099680",
+            "price": 35995,
+            "lastOfYear": 14,
+            "extWidth": 8.2,
+            "extHeight": 13,
+            "newUsed": 15
+       },
+        {
+            "img": "https://assets-cdn-interactrv.netdna-ssl.com/interactrv/unit_tech_drawing/imgh_800x600-m1016202013184607/unit_tech_drawing_202010160118460698594900.jpg",
+            "img2": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0615202120523606/unit_photo_202106150852360621857498.jpg",
+            "img3": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0615202120524951/unit_photo_202106150852495119498545.jpg",
+            "type": "Fifth Wheel",
+            "name": "New 2021 Palomino Puma 257RESS",
+            "url": "https://www.asrvcenter.com/product/new-2021-palomino-puma-257ress-1532612-5",
+            "selection": "Price: $33,995",
+            "location": "Midland, MI",
+            "vinNum": "4X4FPUB26MP091651",
+            "price": 33995,
+            "lastOfYear": 21,
+            "extWidth": 8,
+            "extHeight": 12.2,
+            "newUsed": 30
+       },
+        {
+            "img": "https://assets-cdn-interactrv.netdna-ssl.com/interactrv/unit_tech_drawing/imgh_800x600-m0119202118472046/unit_tech_drawing_202101190647204636157852.jpg",
+            "img2": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0702202118441736/unit_photo_202107020644173655335331.jpg",
+            "img3": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0702202118443809/unit_photo_202107020644380883596681.jpg",
+            "type": "Fifth Wheel",
+            "name": "New 2021 Palomino Puma 295BHSS",
+            "url": "https://www.asrvcenter.com/product/new-2021-palomino-puma-295bhss-1554180-5",
+            "selection": "Price: $34,995",
+            "location": "Midland, MI",
+            "vinNum": "4X4FPUF22MP092161",
+            "price": 34995,
+            "lastOfYear": 21,
+            "extWidth": 8,
+            "extHeight": 12.4,
+            "newUsed": 30
+       },
+        {
+            "img": "https://assets-cdn-interactrv.netdna-ssl.com/interactrv/unit_tech_drawing/imgh_800x600-m1216201616252722/unit_tech_drawing_201612160425272247643839.jpg",
+            "img2": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0924202116154381/unit_photo_202109240415438160885946.jpg",
+            "img3": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0924202116155044/unit_photo_202109240415504477588572.jpg",
+            "type": "PopUp",
+            "name": "Used 2017 Coachmen RV Clipper Camping Trailers 1285SST Classic",
+            "url": "https://www.asrvcenter.com/product/used-2017-coachmen-rv-clipper-camping-trailers-1285sst-classic-1621311-7",
+            "selection": "Price: $13,995",
+            "location": "Auburn Hills, MI",
+            "vinNum": "5ZT1CPGC2H5012216",
+            "price": 13995,
+            "lastOfYear": 17,
+            "extWidth": 7,
+            "extHeight": 4.7,
+            "newUsed": 15
+       },
+        {
+            "img": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0525202115235976/unit_photo_20210525032359766722934.jpg",
+            "img2": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0525202115240583/unit_photo_2021052503240583559348.jpg",
+            "img3": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0525202115244904/unit_photo_202105250324490438142134.jpg",
+            "type": "MotorHome Class A",
+            "name": "Used 2001 Four Winds RV HURRICANE 30Q",
+            "url": "https://www.asrvcenter.com/product/used-2001-four-winds-rv-hurricane-30q-1517407-9",
+            "selection": "Price: $13,995",
+            "location": "Midland, MI",
+            "vinNum": "5B4KP37J6Y3320718",
+            "price": 13995,
+            "lastOfYear": 15,
+            "extWidth": 8,
+            "extHeight": 12,
+            "newUsed": 15
+       },
+        {
+            "img": "https://assets-cdn-interactrv.netdna-ssl.com/interactrv/unit_tech_drawing/imgh_800x600-m1202201616043896/mod55901.jpg",
+            "img2": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0927202114240406/unit_photo_202109270224040642329935.jpg",
+            "img3": "https://assets-cdn-interactrv.netdna-ssl.com/asrvcenter/unit_photo/imgh_800x600-wm_asrvcenter_com-m0927202114240656/unit_photo_202109270224065645954500.jpg",
+            "type": "MotorHome Class C",
+            "name": "Used 2014 Forest River RV Sunseeker 3170DS Ford",
+            "url": "https://www.asrvcenter.com/product/used-2014-forest-river-rv-sunseeker-3170ds-ford-1622445-16",
+            "selection": "Price: $59,995",
+            "location": "Midland, MI",
+            "vinNum": "1FDXE4FS0DDA78742",
+            "price": 59995,
+            "lastOfYear": 14,
+            "extWidth": 8.5,
+            "extHeight": 11.3,
+            "newUsed": 15
+       },
+    ]
+}
+
+buildTable(myArray)
+
+function buildTable(data) {
+    var table = document.getElementById('myTable')
+    
+    for (var i = 0; i < myArray.campers.length; i++) {
+        
+        let grade = ""
+        let price = myArray.campers[i].price
+        
+        var calcPrice =
+            myArray.campers[i].extHeight +
+            myArray.campers[i].extWidth +
+            myArray.campers[i].newUsed +
+            myArray.campers[i].lastOfYear
+        calcPrice = calcPrice.toFixed(0)
+
+        let finPrice = (price / calcPrice)
+        finPrice = finPrice.toFixed(0)
+
+        //grade calculator
+        if (finPrice >= 600) {
+            grade = "A"
+        } else if (finPrice >= 550) {
+            grade = "B"
+        } else if (finPrice >= 450) {
+            grade = "C"
+        } else if (finPrice >= 350) {
+            grade = "D"
+        } else if (finPrice >= 250) {
+            grade = "F"
+        } else {
+            console.log("Error: check grade calculator")
+        }
+
+        var row = (
+            `<tr>
+                <td id="tdTitle" colspan=3><a href=${myArray.campers[i].url}>${myArray.campers[i].type} ${myArray.campers[i].name}</a></td>
+            </tr>
+            <tr>
+                <td id="tdTitle" colspan=3>${myArray.campers[i].selection}</td>
+            </tr>
+            <tr>
+                <td>Located in ${myArray.campers[i].location}</td>
+                <td>VIN: ${myArray.campers[i].vinNum}</td>            
+                <td class="tdGrade">Grade: ${grade}</td>
+            </tr>
+            <tr>
+                <td><img src=${myArray.campers[i].img} alt=""></td>
+                <td><img src=${myArray.campers[i].img2} alt=""></td>
+                <td><img src=${myArray.campers[i].img3} alt=""></td>
+            </tr>
+            <tr>
+            </tr>`
+        )
+        
+        table.innerHTML += row
+        
+        //conditional formatting for cell that displays grade
+        var cell = document.getElementsByClassName("tdGrade")
+        var cellGrade = cell.item(i)
+        var x = cellGrade.innerHTML;
+        if(x == "Grade: A"){
+            cellGrade.style.color='#38f244'
+        }else if(x == "Grade: B"){
+            cellGrade.style.color='#a4f238'
+        }else if(x == "Grade: C"){
+            cellGrade.style.color='#ecf238'
+        }else if(x == "Grade: D"){
+            cellGrade.style.color='#e87638'
+        }else if(x == "Grade: F"){
+            cellGrade.style.color='#e85538'
+        }else{
+            console.log("ERROR: check conditional formatting code")
+        }
+    }
+}
